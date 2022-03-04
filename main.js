9123f92653a903f80020a50e80ce740d28a0f2ec
LisaB1994 <79035700491@yandex.ru>
2022-03-04 18:10:20 +0300

    правки

--- main.js
+++
@@ -1,8 +0,0 @@
-const menuToggle = document.querySelector(".menu-toggle");
-const menu = document.querySelector(".nested-navigation-wrapper");
-
-menuToggle.addEventListener("click", (e) => {
-  e.preventDefault();
-  menuToggle.classList.toggle("is-active");
-  menu.classList.toggle("is-active");
-});
